7f615c6aea941dd6f8f0db45144f33c2352c4b31
HyeL99 <kanghr8149@gmail.com>
2022-12-19 11:53:00 +0900

    새로고침시 페이지 안나오던 오류 수정

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, useLocation } from 'react-router-dom';
 import App from './App';
 import store from './redux/store';
+
+const {pathname} = useLocation();
+
+useEffect(()=>{
+  window.scrollTo(0,0);
+},[pathname])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
